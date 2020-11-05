a828a38c5556eaf5acb3e828008c8c68b0a987f3
Rafael Oliveira <52896767+notsapinho@users.noreply.github.com>
2020-11-05 13:52:09 -0300

    Create theme.meta.js

---
+++ theme.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Instagram Purple Theme
+// @homepageURL  https://github.com/notsapinho/instagram-purple-theme
+// @namespace    http://tampermonkey.net/
+// @downloadURL  https://raw.githubusercontent.com/notsapinho/instagram-purple-theme/main/theme.user.js
+// @updateURL    https://raw.githubusercontent.com/notsapinho/instagram-purple-theme/main/theme.user.js
+// @version      0.0.3
+// @description  Adds a theme to instagram.
+// @author       notsapinho
+// @match        https://www.instagram.com/*
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
